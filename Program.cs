452f07abd1b7da5a0990d19c904cfd7b0babf9db
Dino <dino.omicevic@elev.ga.ntig.sem>
2024-03-25 08:42:58 +0100

    typ start

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿if (3 <= 6)
+{
+    Console.WriteLine("Hello World");
+}
+
+
+
+Console.WriteLine("Vad heter du");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Vad är ditt lösenord");
+
+string pass = Console.ReadLine();
+
+
+if (name == "noname" && pass == "nopass")
+{
+    Console.WriteLine("Welcome");
+}
+
+
+else
+{
+    Console.WriteLine("Fel lösenord eller användarnamn");
+}
+
+
+int i = 0;
+
+while (i < 36)
+{
+    i++;
+    Console.WriteLine("Hello World");
+}
+
+
+
+
+Console.ReadLine();
